105da6f31bd83b22170b1ad9df2344613ff80eca
Parth Bhatia <pbhatia_be23@thapar.edu>
2025-04-24 18:20:29 +0530

    Create rock.java
    basics

---
+++ loopExample.java
@@ -0,0 +1,15 @@
+
+public class loopExample {  
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println("Using loops");
+        while(i < 1000)  
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
+
+
